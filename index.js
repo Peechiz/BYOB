c1d7b2e69340ccfb79b4a1db57cbc586155016e7
Chris Impicciche <peechiz@bakudan.local>
2016-08-05 15:14:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const remote = require('electron').remote;
+const main = remote.require('./main.js')
+
+let button = document.createElement('button')
+
+button.addEventListener('click', () => {
+  main.openWindow();
+},false)
+
+button.textContent = 'open window';
+document.body.appendChild(button)
