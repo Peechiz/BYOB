aae9ea283e9235436b9e8e62448703be64f63da4
Erikang20 <erikang20@gmail.com>
2016-08-06 12:36:51 -0500

    add devTools

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const { app, BrowserWindow } = electron;
 
 app.on( 'ready', () => {
     let win = new BrowserWindow( { width: 800, height: 600 } );
-    win.loadURL( `file://${__dirname}/index.html` )
+    win.loadURL( `file://${__dirname}/index.html` );
+    win.openDevTools();
+
 } )
 
 exports.openWindow = () => {
     let win = new BrowserWindow( { width: 400, height: 200 } )
-    win.loadURL( `file://${__dirname}/index.html` )
+    win.loadURL( `file://${__dirname}/public/partials/profile.html` );
+    win.openDevTools();
 }
--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-console.log( "testing" );
+var app = angular.module( "myApp", [ 'ngRoute' ] );
+//
+// app.config( function ( $routeProvider ) {
+//     $routeProvider.when( '/', {
+//         templateUrl: 'partials/home.html',
+//         controller: 'HomeController'
+//     } ).
+//     when( '/profile', {
+//         templateUrl: 'partials/profile.html',
+//         controller: 'ProfileController'
+//     } );
+// } );
