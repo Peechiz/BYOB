a5bf8edcc4acbca4e033952ad3c28e14c85305b1
Erikang20 <erikang20@gmail.com>
2016-08-06 16:58:47 -0500

    style

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const remote = require('electron').remote;
-const main = remote.require('./main.js')
+const remote = require( 'electron' ).remote;
+const main = remote.require( './main.js' )
 
-let button = document.createElement('button')
+let button = document.createElement( 'button' )
 
-button.addEventListener('click', () => {
-  main.openWindow();
-},false)
+button.addEventListener( 'click', () => {
+    main.openWindow();
+}, false )
 
 button.textContent = 'open window';
-document.body.appendChild(button)
+document.body.appendChild( button )
