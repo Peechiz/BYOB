73054412c3f9620499d7a42786316c66de7d0bab
Erikang20 <erikang20@gmail.com>
2016-08-08 14:32:44 -0500

    deletre button

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 const electron = require( 'electron' );
 const { app, BrowserWindow } = electron;
 
@@ -8,8 +7,7 @@ app.on( 'ready', () => {
     let win = new BrowserWindow( { width: 900, height: 800 } );
     win.loadURL( `file://${__dirname}/index.html` );
     win.openDevTools();
-
-} )
+} );
 
 exports.openWindow = () => {
     let win = new BrowserWindow( { width: 900, height: 800 } )
---
+++ public/angular.js
@@ -0,0 +1,26 @@
+var app = angular.module( "myApp", [ 'ngRoute' ] );
+
+
+console.log( "angular here" );
+
+app.config( function ( $routeProvider ) {
+    $routeProvider.when( '/', {
+        templateUrl: 'public/partials/home.html',
+        controller: 'HomeController'
+    } )
+
+    .when( '/profile', {
+        templateUrl: 'public/partials/profile.html',
+        controller: 'ProfileController'
+    } );
+} );
+
+app.controller( 'HomeController', function ( $scope ) {
+    console.log( 'hey' );
+    $scope.view = {};
+    $scope.view.message = "testing here!"
+} );
+
+app.controller( "ProfileController", function ( $scope ) {
+    console.log( "profile here" );
+} );
