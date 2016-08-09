77270fa7b64f1f4df56043591b35701f7337f25c
Erikang20 <erikang20@gmail.com>
2016-08-08 19:32:07 -0500

    angular working in profile page

--- public/angular.js
+++ public/angular.js
@@ -1,28 +1,28 @@
-// var app = angular.module( "myApp", [ 'ngRoute' ] );
-// //
-// console.log( "angular here" );
-// //
-// app.config( function ( $routeProvider ) {
-//     $routeProvider.when( '/', {
-//         templateUrl: 'partials/home.html',
-//         controller: 'HomeController'
-//     } )
+var app = angular.module( "myApp", [ 'ngRoute' ] );
 //
-//     .when( '/profile', {
-//         templateUrl: 'partials/profile.html',
-//         controller: 'ProfileController'
-//     } );
-// } );
+console.log( "angular here" );
 //
-// app.controller( 'HomeController', function ( $scope ) {
-//     console.log( 'hey' );
-//     $scope.view = {};
-//     $scope.view.message = "testing here!"
-// } );
-//
-// app.controller( "ProfileController", function ( $scope ) {
-//     console.log( "profile here" );
-//     $scope.view = {};
-//     $scope.view.message = "testing profile!"
-//
-// } );
+app.config( function ( $routeProvider ) {
+    $routeProvider.when( '/', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeController'
+    } )
+
+    .when( '/profile', {
+        templateUrl: '/partials/profile.html',
+        controller: 'ProfileController'
+    } );
+} );
+
+app.controller( 'HomeController', function ( $scope ) {
+    console.log( 'hey' );
+    $scope.view = {};
+    $scope.view.message = "testing here!"
+} );
+
+app.controller( "ProfileController", function ( $scope ) {
+    console.log( "profile here" );
+    $scope.view = {};
+    $scope.view.message = "testing profile!"
+
+} );
