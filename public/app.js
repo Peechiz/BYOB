54695e06563651c45f30774db860109be6aefd44
Erikang20 <erikang20@gmail.com>
2016-08-09 15:31:07 -0500

    add ajx

--- public/app.js
+++ public/app.js
@@ -1,3 +1,13 @@
+$.ajax( {
+    url: "http://api.brewerydb.com/v2/?key= 4d9a0f8e023cb078745503782cabc979",
+    dataType: "jsonp",
+    success: function ( parsed_json ) {
+        var beer_name = parsed_json[ 'beer_name' ];
+        var brewery = parsed_json[ 'brewery' ];
+
+    }
+} );
+
 $( document ).ready( function () {
     Materialize.updateTextFields();
 } );
