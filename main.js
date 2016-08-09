d4595f28bc3159d17aa839f069cfa80dca9e708a
Erikang20 <erikang20@gmail.com>
2016-08-09 10:46:32 -0500

    social buttons

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const { app, BrowserWindow } = electron;
 app.on( 'ready', () => {
     let win = new BrowserWindow( { width: 900, height: 800 } );
     win.loadURL( `file://${__dirname}/public/index.html` );
-    win.openDevTools();
+    //  win.openDevTools();
 } );
 
 
 exports.openWindow = () => {
     let win = new BrowserWindow( { width: 900, height: 800 } )
     win.loadURL( `file://${__dirname}/public/profile.html` );
-    win.openDevTools();
+    //  win.openDevTools();
 }
